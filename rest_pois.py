b8f335e0edd5acb146a9cd433453c8adf9310a9d
pengzhan <pengzhan@cisco.com>
2024-01-20 20:11:45 +0800

    update

--- rest_pois.py
+++ rest_pois.py
@@ -7,10 +7,15 @@ from fastapi import FastAPI, UploadFile, File, APIRouter
 from uvicorn import Config, Server
 import asyncio
 import argparse
-from fastapi import Request
+from fastapi import Request, HTTPException
 from io import StringIO
+import aiohttp
 app = typer.Typer()
 router = APIRouter()
+# poi_rest_url = "http://aliant.homebrew.v6.rocks:8000/api/v1/poi_rest"
+poi_rest_url = "http://localhost:8341/api/v1/poi_rest"
+poi_api_key = "e813774a4aea1a0b6ca95f16bcd36cd4"
+
 def get_location(address: str, api_key: str) -> str:
     url = "https://restapi.amap.com/v3/geocode/geo"
     params = {"address": address, "output": "JSON", "key": api_key}
@@ -48,6 +53,8 @@ async def process_csv(input_file: UploadFile = File(...),
     
     参数:
     """
+    global poi_api_key
+    api_key = poi_api_key
     infile = await input_file.read()
     #如果end_line==0，那么就是到最后一行
     if end_line == 0:
@@ -76,13 +83,90 @@ async def process_csv(input_file: UploadFile = File(...),
         #把生成的结果以output.csv的文件名返回给调用者
         return FileResponse("output.csv", filename="result.csv")
 
+@router.post("/poi_rest")
+async def poi_rest(
+    data: dict,
+    # request: Request,
+):
+    result = []
+    for address in data["address_list"]:
+        location = get_location(address, data["api_key"])
+        if location:
+            pois = get_pois(location, data["api_key"])
+            for poi_id, poi_name in pois:
+                result.append([address, poi_id, poi_name])
+
+    return {
+        "data": result,
+        "status": "ok"
+    }
+@router.post("/poi_rest_relay")
+async def poi_rest_relay(input_file: UploadFile = File(...), 
+                      api_key: str = "e813774a4aea1a0b6ca95f16bcd36cd4", 
+                      start_line: int = 0, 
+                      end_line: int = 0 
+                      ):
+    """
+    处理一个包含地址的CSV文件，使用高德地图的AMap API获取它们的位置，并将相应的兴趣点（POIs）写入输出的CSV文件。
+    高德地图免费API申请地址：https://lbs.amap.com/dev/key/app，添加的API类型为Web服务-Web服务API。
+    该API每天有5000次的免费调用次数，超过后会返回错误信息。
+    本程序每查询一个地址，就会消耗一次调用次数，所以请谨慎使用。
+    
+    参数:
+    """
+    global poi_api_key
+    api_key = poi_api_key
+    address_list=[]
+    infile = await input_file.read()
+    #如果end_line==0，那么就是到最后一行
+    if end_line == 0:
+        end_line = sum(1 for line in StringIO(infile.decode("utf-8")))
+    with open("output.csv", mode='w', encoding='utf-8', newline='') as outfile:
+
+       #如果start_line==0，那么就是从第一行开始
+        if start_line > 0:
+            start_line = start_line - 1
+        
+        print(f'处理文件{input_file.filename}的{start_line + 1:4d}到{end_line:4d}行的数据')
+             
+        reader = csv.reader(StringIO(infile.decode("utf-8")))
+        writer = csv.writer(outfile)
+        for i, row in enumerate(reader):
+            if start_line <= i < end_line:
+                address = row[0]
+                address_list.append(address)
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(
+                    poi_rest_url,
+                    headers={"Content-Type": "application/json"},
+                    json={
+                        "address_list": address_list,
+                        "api_key": api_key,
+                        }
+                ) as response:
+                    response.raise_for_status()
+                    poi_list = (await response.json())["data"]
+                for item in poi_list:
+                    # writer.writerow([item])
+                    print(item)
+                    writer.writerow(item)
+
+        except aiohttp.ClientError as error:
+            raise HTTPException(status_code=500, detail=error)
+
+        #把生成的结果以output.csv的文件名返回给调用者
+
+        return FileResponse("output.csv", filename="result.csv")
 #生成一个简单的web页面，用于上传文件，submit按钮调用上面的process_csv函数
 #美化布局下面的HTML页面，标题为“高德地图POI批量转换工具”，居中显示，字体大小为24px；两个按钮也居中显示，字体大小为18px；上传文件的按钮宽度为200px，高度为50px，字体大小为18px；submit按钮宽度为100px，高度为30px，字体大小为18px。
 #加入两个示例表格，分别为input.csv和output.csv，用于测试上传文件和下载文件功能。
 @router.get("/upload")
 async def upload(request: Request):
     process_csv_url = request.url_for("process_csv")
+    relay_rest_url = request.url_for("poi_rest_relay")
     print(process_csv_url)
+    print(relay_rest_url)
     content1 = """
     <html>
         <head>
@@ -117,7 +201,13 @@ async def upload(request: Request):
     content = content1 + f"""
         <body>
             <h1>高德地图批量查询POI工具</h1>
+            <form action="{relay_rest_url}" enctype="multipart/form-data" method="post">
+                <i>境外部署模式</i>
+                <input name="input_file" type="file" multiple>
+                <input type="submit">
+            </form>
             <form action="{process_csv_url}" enctype="multipart/form-data" method="post">
+                <i>境内部署模式</i>
                 <input name="input_file" type="file" multiple>
                 <input type="submit">
             </form>
@@ -139,13 +229,22 @@ async def upload(request: Request):
     return HTMLResponse(content=content)
 
 def main():
+    global poi_rest_url
     parser = argparse.ArgumentParser(description='')
     parser.add_argument("--host", '-H', type=str,
                         default="0.0.0.0", help="Host IP address")
     parser.add_argument("--port", '-P', type=int,
                         default=8341, help="Port number")
+    parser.add_argument("--poi-url", '-U', type=str,
+                        default="http://aliant.homebrew.v6.rocks:8000/api/v1/poi_rest", help="POI rest url")
+    parser.add_argument("--api-key", '-K', type=str,
+                        default="e813774a4aea1a0b6ca95f16bcd36cd4", help="POI API key")
     args = parser.parse_args()
     rest_app = FastAPI()
+    poi_api_key = args.api_key
+    poi_rest_url = args.poi_url
+    print(f"POI rest url: {poi_rest_url}")
+    print(f'POI API key: {poi_api_key}')
     loop = asyncio.get_event_loop()
     rest_app.include_router(router,
                        prefix="/api/v1",
