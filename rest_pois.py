a41667c3b93c608a986181bd7eda0981de38e530
pengzhan <pengzhan@cisco.com>
2024-01-22 19:32:15 +0800

    update

--- rest_pois.py
+++ rest_pois.py
@@ -1,4 +1,4 @@
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse, StreamingResponse
 import typer
 import csv
 import requests
@@ -16,29 +16,39 @@ router = APIRouter()
 poi_rest_url = "http://localhost:8341/api/v1/poi_rest"
 poi_api_key = "e813774a4aea1a0b6ca95f16bcd36cd4"
 
-def get_location(address: str, api_key: str) -> str:
+async def get_location(address: str, api_key: str) -> str:
     url = "https://restapi.amap.com/v3/geocode/geo"
     params = {"address": address, "output": "JSON", "key": api_key}
-    response = requests.get(url, params=params)
-    if response.status_code == 200 and response.json()["status"] == "1":
-        return response.json()["geocodes"][0].get("location", "")
-    else:
-        print(f"Error: {response.status_code} - {response.json().get('info', '')}")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, ssl=False) as response:
+            if response.status == 200:
+                data = await response.json()
+                if data["status"] == "1":
+                    return data["geocodes"][0].get("location", "")
+                else:
+                    print(f"Error: {data.get('info', '')}")
+            else:
+                print(f"Error: {response.status}")
     return ""
 
-def get_pois(location: str, api_key: str) -> List[Tuple[str, str]]:
+async def get_pois(location: str, api_key: str) -> List[Tuple[str, str]]:
     url = "https://restapi.amap.com/v3/geocode/regeo"
     params = {"location": location, "output": "JSON", "key": api_key, "extensions": "all"}
-    response = requests.get(url, params=params)
-    if response.status_code == 200 and response.json()["status"] == "1":
-        return [(poi["id"], poi["name"]) for poi in response.json()["regeocode"]["pois"]]
-    else:
-        print(f"Error: {response.status_code} - {response.json().get('info', '')}")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, ssl=False) as response:
+            if response.status == 200:
+                data = await response.json()
+                if data["status"] == "1":
+                    return [(poi["id"], poi["name"]) for poi in data["regeocode"]["pois"]]
+                else:
+                    print(f"Error: {data.get('info', '')}")
+            else:
+                print(f"Error: {response.status}")
     return []
 
 #生成下面函数的测试curl命令：
-#curl -X POST "http://127.0.0.1:8341/api/v1/process_csv" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "input_file=@input.csv"
-#curl -X POST "https://dmit-lax-netflix.homebrew.v6.rocks/api/v1/process_csv" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "input_file=@input.csv"
+#curl POST -v "http://127.0.0.1:8341/api/v1/process_csv" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "input_file=@input.csv"
+#curl POST -v "https://dmit-lax-netflix.homebrew.v6.rocks/api/v1/process_csv" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "input_file=@input.csv"
 @router.post("/process_csv")
 async def process_csv(input_file: UploadFile = File(...), 
                       api_key: str = "e813774a4aea1a0b6ca95f16bcd36cd4", 
@@ -59,29 +69,22 @@ async def process_csv(input_file: UploadFile = File(...),
     #如果end_line==0，那么就是到最后一行
     if end_line == 0:
         end_line = sum(1 for line in StringIO(infile.decode("utf-8")))
-    with open("output.csv", mode='w', encoding='utf-8', newline='') as outfile:
 
-       #如果start_line==0，那么就是从第一行开始
-        if start_line > 0:
-            start_line = start_line - 1
+    async def generate_csv():
         
         print(f'处理文件{input_file.filename}的{start_line + 1:4d}到{end_line:4d}行的数据')
              
         reader = csv.reader(StringIO(infile.decode("utf-8")))
-        writer = csv.writer(outfile)
         for i, row in enumerate(reader):
             print(f'查询第{i+1:4d}行的地址: {row[0]}')
-            if start_line <= i < end_line:
-                address = row[0]
-                location = get_location(address, api_key)
-                if location:
-                    pois = get_pois(location, api_key)
-                    for poi_id, poi_name in pois:
-                        writer.writerow([address, poi_id, poi_name])
-        #把生成的output.csv文件返回给调用者
-        # return FileResponse("output.csv")
-        #把生成的结果以output.csv的文件名返回给调用者
-        return FileResponse("output.csv", filename="result.csv")
+            address = row[0]
+            location = await get_location(address, api_key)
+            if location:
+                pois = await get_pois(location, api_key)
+                for poi_id, poi_name in pois:
+                    yield f"{address},{poi_id},{poi_name}\n"
+
+    return StreamingResponse(generate_csv(), media_type="text/csv", headers={"Content-Disposition": "attachment; filename=result.csv"})
 
 @router.post("/poi_rest")
 async def poi_rest(
@@ -165,9 +168,9 @@ async def poi_rest_relay(input_file: UploadFile = File(...),
 async def upload(request: Request):
     process_csv_url = request.url_for("process_csv")
     relay_rest_url = request.url_for("poi_rest_relay")
-    print(process_csv_url)
-    print(relay_rest_url)
+    print(f'POI转换url: {process_csv_url}')
     content1 = """
+    <!doctype html>
     <html>
         <head>
             <title>高德地图POI批量转换工具</title>
@@ -201,13 +204,7 @@ async def upload(request: Request):
     content = content1 + f"""
         <body>
             <h1>高德地图批量查询POI工具</h1>
-            <form action="{relay_rest_url}" enctype="multipart/form-data" method="post">
-                <i>境外部署模式</i>
-                <input name="input_file" type="file" multiple>
-                <input type="submit">
-            </form>
             <form action="{process_csv_url}" enctype="multipart/form-data" method="post">
-                <i>境内部署模式</i>
                 <input name="input_file" type="file" multiple>
                 <input type="submit">
             </form>
@@ -250,6 +247,13 @@ def main():
                        prefix="/api/v1",
                        tags=["api"],
                        responses={404: {"description": "Not found"}})
+    #增加uvicorn config对unixi.socket监听的支持
+    config = Config(app=rest_app,
+                    loop=loop,
+                    port=args.port,
+                    uds="/tmp/uvicorn.sock",
+                    host=args.host,
+                    )
     server = Server(Config(app=rest_app,
                            loop=loop,
                            port=args.port,
